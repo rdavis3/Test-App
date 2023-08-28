b72007f5c5af845a8d91f0ba4c2be844ecf5d067
rdavis3 <rmdavisiii@gmail.com>
2023-08-27 22:48:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<html><body><h1>Node.js on a handshake name!</h1></body></html>');
+  res.send('<html><body><h1>Hello World!</h1></body></html>');
 });
 
 app.listen(port, () => {
