ad67ea4935c1aaaeba471cd575fd7ba88cfd08fc
rdavis3 <rmdavisiii@gmail.com>
2023-11-14 18:38:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Handshake Info</title>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-          }
-          header {
-            background-color: black;
-            color: white;
-            text-align: center;
-            padding: 10px 0;
-            border-bottom: 3px solid white;
-          }
-          footer {
-            background-color: black;
-            color: white;
-            text-align: center;
-            padding: 20px 0;
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-          }
-          /* Additional CSS styles here */
-        </style>
-      </head>
-      <body>
-        <header>
-          <h1>Welcome to Handshake Information Page</h1>
-        </header>
-        <p>Handshake is a decentralized, permissionless naming protocol...</p>
-        <!-- More information about Handshake here -->
-        <footer>
-          <p>&copy; 2023 Handshake Info. An experimental page built with AI.</p>
-          <!-- Additional footer content here -->
-        </footer>
-      </body>
-    </html>
-  `);
+document.addEventListener("DOMContentLoaded", function() {
+    fetchNamebaseData();
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+function fetchNamebaseData() {
+    // Replace with the correct API endpoint and add any required headers/parameters
+    fetch('https://api.namebase.io/api/domain-sales')
+        .then(response => response.json())
+        .then(data => {
+            createChart(data);
+        })
+        .catch(error => console.error('Error fetching Namebase data:', error));
+}
+
+function createChart(data) {
+    const ctx = document.getElementById('salesChart').getContext('2d');
+    const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line', // or 'bar', 'pie', etc., depending on your data
+
+        // The data for our dataset
+        data: {
+            labels: data.map(sale => sale.domain), // Assuming 'domain' is a property in your data
+            datasets: [{
+                label: 'Domain Sales',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: data.map(sale => sale.price) // Assuming 'price' is a property in your data
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+}
 
 
